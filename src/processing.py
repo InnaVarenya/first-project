431dab55751789bc80597d15a967432c19f8b245
Inna Varenya <ioannavarenya@gmail.com>
2025-09-11 05:33:57 +0300

    Files README.md and processing.py added

---
+++ src/processing.py
@@ -0,0 +1,12 @@
+def filter_by_state(items: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """ Функция, возвращающая список словарей, в котором ключ 'state' = 'EXECUTED' """
+    list_executed = []
+    for item in items:
+        if item["state"] == state:
+            list_executed.append(item)
+    return list_executed
+
+def sort_by_date(items: list[dict], reverse: bool = True) -> list[dict]:
+    """Функция, сортирующая список словарей по дате, начиная с последних операций """
+    sorted_list = sorted(items, key=lambda x: x["date"], reverse=reverse)
+    return sorted_list
