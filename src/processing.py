78e841ce677bded65daad57898b62357430ab186
Inna Varenya <ioannavarenya@gmail.com>
2025-09-11 06:19:06 +0300

    Correction of design

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,19 @@
 def filter_by_state(items: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """ Функция, возвращающая список словарей, в котором ключ 'state' = 'EXECUTED' """
+    """
+    Функция, возвращающая список словарей,
+    в которых ключ 'state' = 'EXECUTED'
+    """
     list_executed = []
     for item in items:
         if item["state"] == state:
             list_executed.append(item)
     return list_executed
 
+
 def sort_by_date(items: list[dict], reverse: bool = True) -> list[dict]:
-    """Функция, сортирующая список словарей по дате, начиная с последних операций """
+    """
+    Функция, сортирующая список словарей по дате,
+    начиная с последних операций
+    """
     sorted_list = sorted(items, key=lambda x: x["date"], reverse=reverse)
     return sorted_list
