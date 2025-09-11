57dfc5d13db107869b751b7e5fd0ed74d41221d3
Inna Varenya <ioannavarenya@gmail.com>
2025-09-11 13:31:08 +0300

    Bugs fixed

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,13 @@
 def filter_by_state(items: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """
-    Функция, возвращающая список словарей,
-    в которых ключ 'state' = 'EXECUTED'
-    """
-    list_executed = []
+    """Функция, возвращающая список словарей, в которых ключ 'state' = 'EXECUTED'"""
+    executed_item = []
     for item in items:
         if item["state"] == state:
-            list_executed.append(item)
-    return list_executed
+            executed_item.append(item)
+    return executed_item
 
 
 def sort_by_date(items: list[dict], reverse: bool = True) -> list[dict]:
-    """
-    Функция, сортирующая список словарей по дате,
-    начиная с последних операций
-    """
+    """Функция, сортирующая список словарей по дате, начиная с последних операций"""
     sorted_list = sorted(items, key=lambda x: x["date"], reverse=reverse)
     return sorted_list
